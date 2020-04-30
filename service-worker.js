3a4c171532f3e43b462cc1a390c7d7af02733fdc
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2020-04-30 09:10:54 +0100

    addition of portfolio page, navbar & tweaks
    See development branch for detailed changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.894a85c4b87186bc5c02e45042ccca10.js"
+  "/precache-manifest.9b035b6d1b826ed643662191857d5631.js"
 );
 
 self.addEventListener('message', (event) => {
